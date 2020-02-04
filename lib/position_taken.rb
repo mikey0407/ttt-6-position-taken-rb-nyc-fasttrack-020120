27c05ed90570ecfe9b5e811c20f9218c576c5538
Michael Alfaro <michael.s.alfaro@gmail.com>
2020-02-04 02:09:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,4 @@
-def position_taken (board, index)
-  board[index]
- 
- def position_taken?(board, index)
+def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
   else
